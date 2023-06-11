25237a066d3cc4a38c44b4786c7f5b9b5ce46e32
te20ra <likharev.ser@yandex.ru>
2023-06-11 12:05:38 +0300

    4 commit

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ from tkinter import filedialog
 
 FILENAME = ''
 TABLE = {"number": [],
+         "code": [],
          'name': [],
          'count': [],
          'price': [],
          'price_without_nds': [],
          'price_with_nds': [],
          'nds': [],
-         'GTD':[],
-             }
+         'GTD': []}
 def openfile(): # функция для открытия даиалогового окна выбора файла
     global FILENAME
-    FILENAME =filedialog.askopenfilename()
-    label_filename = Label(window, text=f'Выбранный файл: {FILENAME}') # создается текст, где прописан путь к выбранному файлу
-    label_filename.grid(column=1, row=0, padx=(50,0),pady=(50,0))
+    FILENAME = filedialog.askopenfilename()
+    short_filename = FILENAME[FILENAME.rfind('/')+1:]
+    label_filename = Label(window, text=f'Файл: {short_filename}') # создается текст, где прописан путь к
+    # выбранному
+    # файлу
+    label_filename.grid(column=1, row=0)#label_filename.grid(column=1, row=0, padx=(50,0),pady=(50,0))
 
 
 def read_book():
@@ -31,21 +34,18 @@ def read_book():
     print(keys)
     print(column_max, row_max)
     count = 0
-    for col in [1, 2, 4, 5, 6, 8, 9, 11]:
+    for col in [3, 4, 5, 6, 7, 8, 10, 11, 13]:
         key = keys[count]  # выбор ключа
         print(key)
         for row in range(2, row_max + 1):
             cell_letter = str(get_column_letter(col)) + str(row)  # ячейка
-            # print(cell_letter)
             val = sheet_active[cell_letter].internal_value  # значение ячейки
-            # print(val)
             TABLE[key].append(val)  # добавили значение ячейки
         print(TABLE[key])
         count += 1
 
 def stroka():
     lengt = TABLE['number'][-1]
-    print(lengt,'lflfl')
     line ='<ТаблСчФакт>'
     for i in range(lengt):
         line += f'<СведТов НомСтр="{TABLE["number"][i]}" ' \
@@ -56,35 +56,39 @@ def stroka():
                 f'СтТовУчНал="{TABLE["price_with_nds"][i]}"><Акциз><БезАкциз>без ' \
                 f'акциза</БезАкциз></Акциз><СумНал><СумНал>{TABLE["nds"][i]}</СумНал></СумНал>' \
                 f'<СвТД КодПроисх="156" НомерТД="{TABLE["GTD"][i]}" />' \
-                f'<ДопСведТов ПрТовРаб="1" КодТов="274usb2274186" НаимЕдИзм="шт" КрНаимСтрПр="КИТАЙ" НадлОтп="0" /></СведТов> '
+                f'<ДопСведТов ПрТовРаб="1" КодТов="{TABLE["code"][i]}" НаимЕдИзм="шт" КрНаимСтрПр="КИТАЙ" НадлОтп="0" /></СведТов> '
     line += '</ТаблСчФакт>'
     print(line)
-def start():
+    return line
+def start(text_editor1,text_editor2):
     global TABLE
     label_progress = Label(window, text='Пошел процесс')
-    label_progress.grid(column=0, row=2, padx=(50, 0), pady=(50, 0))
+    label_progress.grid(column=1, row=1)#label_progress.grid(column=0, row=2, padx=(50, 0), pady=(50, 0))
     read_book()
-    stroka()
+    line = stroka()
+    text_editor2.insert(1.0, line)
+
 
 window = Tk() # создается окно интрефейса
 window.title("Данные из екселя в текст")
-window.geometry("1920x1080")
+window.geometry("800x1300")
+
+for c in range(10): window.columnconfigure(index=c, weight=10)
+for r in range(10): window.rowconfigure(index=r, weight=10)
 
 
-
-#label_1 = Label(window,text="Выберите Excel файл:")
-#label_1.grid(column=0,row=0, padx=(50,0), pady=(50,0))
-
-button_chose = Button(window,text='Выбрать файл', command=openfile) # создается кнопка с функциее откртия файла
-button_chose.grid(column=0, row=0, padx=(50,0), pady=(50,0))
-
-button_start = Button(window, text='Выполнить',command=start)
-button_start.grid(column=0, row=1, padx=(10,0), pady=(50,0))
-#workbook = openpyxl.load_workbook(FILENAME,data_only=True)
-#sheets_list = workbook.sheetnames
-#sheet_active = workbook[sheets_list[0]]
+button_chose = Button(window,text='Выбрать файл', command=openfile) #создается кнопка с функциее откртия файла
+button_chose.grid(column=0, row=0)  #button_chose.grid(column=0, row=0, padx=(50,0), pady=(50,0))
 
 
+text_editor1 = Text(width=40, height=10, wrap=WORD)
+text_editor1.grid(column=0, row=3, columnspan=2)
+text_editor2 = Text(width=40, height=10, wrap=WORD)
+text_editor2.grid(column=0, row=4, columnspan=2)
+
+
+button_start = Button(window, text='Выполнить', command=lambda: start(text_editor1,text_editor2))
+button_start.grid(column=0, row=1)  #button_start.grid(column=0, row=1, padx=(10,0), pady=(50,0))
 
 window.mainloop()
 path_to_file = 'Копия Модуль 4. Урок 17.xlsx'
